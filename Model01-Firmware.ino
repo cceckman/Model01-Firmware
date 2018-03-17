d2a485529adf9137638e42549a4dab6de3680bb4
Charles Eckman <charles@cceckman.com>
2018-03-17 14:32:24 -0700

    Change layout; make it based on keyboardio

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -113,7 +113,7 @@ enum { MACRO_VERSION_INFO,
   *
   */
 
-enum { QWERTY, NUMPAD, FUNCTION }; // layers
+enum { QWERTY, NUMPAD, FUNCTION}; // layers
 
 /* This comment temporarily turns off astyle's indent enforcement
  *   so we can make the keymaps actually resemble the physical key layout better
@@ -123,50 +123,53 @@ enum { QWERTY, NUMPAD, FUNCTION }; // layers
 const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [QWERTY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+  (___,                 Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+   Key_Tab,             Key_Q, Key_W, Key_E, Key_R, Key_T, LockLayer(NUMPAD),
+   CTL_T(Backtick), Key_A, Key_S, Key_D, Key_F, Key_G,
+   Key_Escape,          Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Home,
+   SFT_T(KeypadLeftParen), Key_Space, Key_Backspace, Key_LeftGui,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
-   ShiftToLayer(FUNCTION)),
+   M(MACRO_ANY),      Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Minus,
+   LockLayer(NUMPAD), Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
+                      Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
+   Key_End,           Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Consumer_AL_Email_Reader,
+   Key_LeftAlt, Key_Escape, Key_Enter, SFT_T(KeypadRightParen),
+   ShiftToLayer(FUNCTION)
+  ),
 
 
   [NUMPAD] =  KEYMAP_STACKED
-  (___, ___, ___, ___, ___, ___, ___,
+  (___,    ___,               ___,       ___,                      ___,                       ___,         ___,
+   ___,    LSHIFT(Key_1), LSHIFT(Key_2), Key_KeypadLeftCurlyBrace, Key_KeypadRightCurlyBrace, Key_Pipe,    ___,
+   ___,    LSHIFT(Key_3), LSHIFT(Key_4), Key_KeypadLeftParen,      Key_KeypadRightParen,      Key_Backtick,
+   ___,    LSHIFT(Key_5), LSHIFT(Key_6), Key_LeftBracket,          Key_RightBracket,          LSHIFT(Key_Backtick),   ___,
+   ___, ___, ___, ___,
+   ___,
+
+   M(MACRO_VERSION_INFO),  ___,  ___,  ___,   ___,   ___, ___,
+   ___,                    ___, Key_7, Key_8, Key_9, ___, ___,
+                           ___, Key_4, Key_5, Key_6, ___, ___,
+   ___,                    ___, Key_1, Key_2, Key_3, ___, ___,
+   ___, Key_0, ___, ___,
+   ___
+  ),
+
+  [FUNCTION] =  KEYMAP_STACKED
+  (___, Key_F1,  Key_F2,Key_F3,  Key_F4, Key_F5, XXX,
    ___, ___, ___, ___, ___, ___, ___,
    ___, ___, ___, ___, ___, ___,
    ___, ___, ___, ___, ___, ___, ___,
    ___, ___, ___, ___,
    ___,
 
-   M(MACRO_VERSION_INFO),  ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___,
-   ___,                    ___, Key_Keypad4, Key_Keypad5,   Key_Keypad6,        Key_KeypadAdd,      ___,
-                           ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_Equals,         ___,
-   ___,                    ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
+   ___, Key_F6,              Key_F7,               Key_F8,               Key_F9,              Key_F10, ___,
+   ___, Consumer_AC_PanLeft, Consumer_AC_ScrollDown, Consumer_AC_ScrollUp, Consumer_AC_PanRight, ___, ___,
+        Key_mouseL,          Key_mouseDn,          Key_mouseUp,          Key_mouseR,           ___, ___,
+   ___, Key_LeftArrow,       Key_DownArrow,        Key_UpArrow,          Key_RightArrow,       ___, ___,
    ___, ___, ___, ___,
-   ___),
-
-  [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
-   ___, Key_Delete, ___, ___,
-   ___,
-
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
-   ___, ___, Key_Enter, ___,
-   ___)
+   ___
+ )
 
 };
 
@@ -271,6 +274,10 @@ void setup() {
   // The order can be important. For example, LED effects are
   // added in the order they're listed here.
   Kaleidoscope.use(
+
+    // Enable quantum keys: Dual-mode
+    &Qukeys,
+
     // The boot greeting effect pulses the LED button for 10 seconds after the keyboard is first connected
     &BootGreetingEffect,
 
