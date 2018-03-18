eb51bb84153e5d1ab11a398e492ddd725d3431e1
Charles Eckman <charles@cceckman.com>
2018-03-17 19:41:12 -0700

    Swap equals and minus
    Space-equals-space is a pretty frequent combination; putting all three
    on the same thumb is not a great setup. Put them on opposite hands
    instead.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -125,14 +125,14 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_Tab,             Key_Q, Key_W, Key_E, Key_R, Key_T, LockLayer(NUMPAD),
    CTL_T(Backtick), Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_Escape,          Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Home,
-   SFT_T(KeypadLeftParen), Key_Space, GUI_T(Escape), Key_Equals,
+   SFT_T(KeypadLeftParen), Key_Space, GUI_T(Escape), Key_Minus,
    ShiftToLayer(FUNCTION),
 
    M(MACRO_ANY),      Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Minus,
    LockLayer(NUMPAD), Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Pipe,
                       Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_End,           Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Consumer_AL_Email_Reader,
-   ALT_T(Minus), CTL_T(Escape), Key_Enter, SFT_T(KeypadRightParen),
+   ALT_T(Equals), CTL_T(Escape), Key_Enter, SFT_T(KeypadRightParen),
    ShiftToLayer(FUNCTION)
   ),
 
